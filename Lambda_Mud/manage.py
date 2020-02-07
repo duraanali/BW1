157ab7b97ffb265bea36312bef6064f36bb6dbd4
Duraan Ali <ali@sahalsolutions.com>
2020-02-07 09:01:57 -0600

    procfile changed:

--- Lambda_Mud/manage.py
+++ Lambda_Mud/manage.py
@@ -5,8 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                          'Lambda_Mud.Lambda_Mud.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Lambda_Mud.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
