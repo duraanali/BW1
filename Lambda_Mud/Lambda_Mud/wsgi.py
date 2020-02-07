31281ccce72b92f31d81487b4e43e4c9efae7253
Duraan Ali <ali@sahalsolutions.com>
2020-02-07 08:47:46 -0600

    procfile changed

--- Lambda_Mud/Lambda_Mud/wsgi.py
+++ Lambda_Mud/Lambda_Mud/wsgi.py
@@ -11,7 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE',
-                      'Lambda_Mud.Lambda_Mud.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Lambda_Mud.settings')
 
 application = get_wsgi_application()
