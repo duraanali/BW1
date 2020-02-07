836ae0db993f9c55f0ddd5d64b333e58b3aabc60
Duraan Ali <ali@sahalsolutions.com>
2020-02-07 08:35:41 -0600

    staticfiles commneted

--- Lambda_Mud/Lambda_Mud/settings.py
+++ Lambda_Mud/Lambda_Mud/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    # 'django.contrib.staticfiles',
     'rest_framework.authtoken',
     'django.contrib.sites',
     'api',
@@ -159,4 +159,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '../static/'
+STATIC_URL = '/static/'
